e27a9671a9afe71449b345dd493126feb5567f25
Durga Prasad Dwarapureddi <durgaprasad@exzatechconsulting.com>
2019-09-20 12:36:52 +0530

    After setting different background colors for each activity, Modified files are WordAdapter constructor and creating object for WordAdapter in each Activity(Numbers, colors,...)

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -45,7 +45,7 @@ public class ColorsActivity extends AppCompatActivity {
 
 
 
-        WordAdapter adapter = new WordAdapter(this,  words);
+        WordAdapter adapter = new WordAdapter(this,  words, R.color.category_colors);
 
         ListView listView = (ListView) findViewById(R.id.list3);
 
